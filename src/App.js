1ee313f8e072c3503c2f6de7996f58dd59845cfe
Sebarach Noteboook <sebastian.sc1990@gmail.com>
2022-07-02 22:21:44 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from 'react'
+import axios from 'axios';
+
 
 function App() {
+  const [datos,setdatos] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get("http://localhost:3000/")
+      .then(res => setdatos(res.data))
+    }, 50000);
+    return () => clearInterval(interval);
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(datos)}
+       {datos.map((value,index)=>{
+        return <h3><a target="_blank" href={value.url}><span>{value.tienda}</span></a>- $ {value.precioParse}</h3>
+      })} 
     </div>
   );
 }
